ae019d039732cc172716baaddd3edae73edf60a5
Aaron <acs4631@gmail.com>
2025-05-27 23:22:56 -0400

    Changed name and file locations

---
+++ Get-NOAAImageData.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPrefererence = "Stop"
+Set-StrictMode -Version latest
+
+$baseUrl = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR" # might be worthwhile changing this to allow other satellites
+$sectors = @("SE", "ga") # SE = Southeast, gm = Gulf of America
+$product = "FireTemperature"
+$outputDir = "C:\Users\acs46\Desktop\Personal Projects\SatelliteTelemetryProject\SatelliteData" #refactor for other products/image files
+
+foreach ($sector in $sectors)
+{
+    $url = "$baseUrl/$sector/$product/latest.jpg"
+    $fileName = "$sector-$product.jpg"
+    $destination = Join-Path $outputDir $fileName
+
+    Invoke-WebRequest -Uri $url -OutFile $destination
+}
