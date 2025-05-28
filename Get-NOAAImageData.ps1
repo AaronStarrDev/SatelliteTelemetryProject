0112614507eb1c09eeb53a85ec3be5c90f95e3dc
Aaron <acs4631@gmail.com>
2025-05-28 19:57:23 -0400

    hard-typing

--- Get-NOAAImageData.ps1
+++ Get-NOAAImageData.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPrefererence = "Stop"
 Set-StrictMode -Version latest
 
-$baseUrl = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR" # might be worthwhile changing this to allow other satellites
-$sectors = @("SE", "ga") # SE = Southeast, gm = Gulf of America
-$product = "FireTemperature"
-$outputDir = "C:\Users\acs46\Desktop\Personal Projects\SatelliteTelemetryProject\SatelliteData" #refactor for other products/image files
+[System.String]$baseUrl = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR" # might be worthwhile changing this to allow other satellites
+[System.String]$product = "FireTemperature"
+[System.String]$outputDir = "C:\Users\acs46\Desktop\Personal Projects\SatelliteTelemetryProject\SatelliteData\$product" #refactor for other products/image files
+[System.Array]$sectors = @("SE", "ga") # SE = Southeast, gm = Gulf of America
 
 foreach ($sector in $sectors)
 {
-    $url = "$baseUrl/$sector/$product/latest.jpg"
-    $fileName = "$sector-$product.jpg"
-    $destination = Join-Path $outputDir $fileName
+    [System.String]$url = "$baseUrl/$sector/$product/latest.jpg"
+    [System.String]$fileName = "$sector-$product.jpg"
+    [System.String]$destination = Join-Path $outputDir $fileName
 
     Invoke-WebRequest -Uri $url -OutFile $destination
 }
