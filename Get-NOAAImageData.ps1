e3fa0fa110f24820853fd18228495956e7b577c0
Aaron <acs4631@gmail.com>
2025-05-28 20:01:24 -0400

    removed direct path

--- Get-NOAAImageData.ps1
+++ Get-NOAAImageData.ps1
@@ -3,8 +3,8 @@ Set-StrictMode -Version latest
 
 [System.String]$baseUrl = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR" # might be worthwhile changing this to allow other satellites
 [System.String]$product = "FireTemperature"
-[System.String]$outputDir = "C:\Users\acs46\Desktop\Personal Projects\SatelliteTelemetryProject\SatelliteData\$product" #refactor for other products/image files
-[System.Array]$sectors = @("SE", "ga") # SE = Southeast, gm = Gulf of America
+[System.String]$outputDir = "$HOME\Desktop\Personal Projects\SatelliteTelemetryProject\SatelliteData\$product" #refactor for other products/image files
+[System.Array]$sectors = @("SE", "ga") # SE = Southeast, ga = Gulf of America
 
 foreach ($sector in $sectors)
 {
