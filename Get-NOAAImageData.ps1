5be8684f7367756f5299f18a2e999914182eb395
Aaron <acs4631@gmail.com>
2025-06-04 19:05:50 -0400

    Update Get-NOAAImageData.ps1
    Added code to interact with Google Vision API and have it output what it thinks the image is. Using the free trial, so Im limited in what type of analysis i have google do on the image unfortunately

--- Get-NOAAImageData.ps1
+++ Get-NOAAImageData.ps1
@@ -1,6 +1,7 @@
 ﻿$ErrorActionPrefererence = "Stop"
 Set-StrictMode -Version latest
 
+# GET NOAA IMAGE
 [System.String]$baseUrl = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR" # might be worthwhile changing this to allow other satellites
 [System.String]$product = "FireTemperature"
 [System.String]$outputDir = "$HOME\Desktop\Personal Projects\SatelliteTelemetryProject\SatelliteData\$product" #refactor for other products/image files
@@ -14,3 +15,56 @@ foreach ($sector in $sectors)
 
     Invoke-WebRequest -Uri $url -OutFile $destination
 }
+
+# GET VISION API TO CHECK IT
+#& gcloud auth application-default set-quota-project glass-cedar-461416-s5
+#& gcloud config set billing/quota_project glass-cedar-461416-s5
+
+$gcpKeyFile = "C:\Users\acs46\Downloads\glass-cedar-461416-s5-1b50fe48781d.json"
+$imagePath = "C:\Users\acs46\Desktop\Personal Projects\SatelliteTelemetryProject\SatelliteData\FireTemperature\SE-FireTemperature.jpg"
+$visionEndpoint = "https://vision.googleapis.com/v1/images:annotate"
+
+# Read image and encode in base64
+$imageBytes = [System.IO.File]::ReadAllBytes($imagePath)
+$imageBase64 = [Convert]::ToBase64String($imageBytes)
+
+# Construct request body
+$body = @{
+    requests = @(
+        @{
+            image = @{ content = $imageBase64 }
+            features = @(
+                @{
+                    type = "LABEL_DETECTION"
+                    maxResults = 10
+                }
+            )
+        }
+    )
+} | ConvertTo-Json -Depth 10
+
+try
+{
+    # Send POST request to Google Vision API
+    $gcloudToken = & gcloud auth application-default print-access-token
+    $headers = @{ "Authorization" = "Bearer $gcloudToken"; "x-goog-user-project" = "glass-cedar-461416-s5"}
+    $response = Invoke-WebRequest `
+    -Method POST `
+    -Headers $headers `
+    -Body $body `
+    -ContentType: "application/json" `
+    -Uri $visionEndpoint
+}
+catch
+{
+    $_.Exception.Response | % {
+        $_.GetResponseStream() | % {
+            $reader = New-Object System.IO.StreamReader($_)
+            $reader.ReadToEnd()
+        }
+    }
+}
+
+# Parse and display labels
+$contentJson = $response.content | ConvertFrom-Json
+$json.responses.labelAnnotations
